b59ed3cd8d0705d226f8d61ccd1498a9121db332
Ashraf Hossain Akash <nirjhorakash07@gmail.com>
2026-08-14 19:25:02 +0600

    Mongo db connection set up for backend and room post api is craeted

--- index.js
+++ index.js
@@ -17,6 +17,47 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //my collections
+    const db = client.db(process.env.MONGO_DB_NAME);
+    // collections for api
+    const roomsCollection = db.collection("rooms");
+
+
+    //post api for rooms
+    app.post("/rooms", async (req, res) => {
+      const room = req.body;
+      const result = await roomsCollection.insertOne(room);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
